741fd195315df0b11e2775d4f1c0fefa3eb8866c
Richard Neumann <mail@richard-neumann.de>
2019-02-18 16:08:27 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@
 from distutils.core import setup
 
 setup(
-    name='mcipc',
+    name='wgtools',
     author='Richard Neumann',
     author_email='<mail at richard dash neumann dot de>',
     py_modules=['wgtools'],
-    scripts=['files/rconclt', 'files/rconshell', 'files/srvwrap-test'],
-    url='https://gitlab.com/coNQP/mcipc',
+    url='https://gitlab.com/coNQP/wgtools',
     license='GPLv3',
-    description='A Minecraft server inter-process communication library.')
+    description='Python bindings for wireguard-tools.')
