977cc098bdbf4373588bb7c9ea41bd21c1515dab
Richard Neumann <r.neumann@homeinfo.de>
2020-02-26 10:38:27 +0100

    Added show().

--- wgtools.py
+++ wgtools.py
@@ -1,13 +1,29 @@
 """Python bindings for WireGuard."""
 
+from os import linesep
 from subprocess import check_output
 from typing import NamedTuple
 
 
-__all__ = ['WG', 'Keypair', 'genkey', 'pubkey', 'keypair', 'genpsk']
+__all__ = [
+    'WG',
+    'Keypair',
+    'genkey',
+    'pubkey',
+    'keypair',
+    'genpsk',
+    'show'
+]
 
 
 WG = '/usr/bin/wg'
+
+
+class Keypair(NamedTuple):
+    """A public / private key pair."""
+
+    public: str
+    private: str
 
 
 def genkey(*, _wg=WG):
@@ -36,8 +52,66 @@ def genpsk(*, _wg=WG):
     return check_output((_wg, 'genpsk'), text=True).strip()
 
 
-class Keypair(NamedTuple):
-    """A public / private key pair."""
+def _parse_interface(text):
+    """Parses interface information from the given text."""
 
-    public: str
-    private: str
+    interface = {'peers': {}}
+    peer = None
+
+    for line in text.split(linesep):
+        key, value = line.strip().split(': ')
+
+        if key == 'peer':
+            interface['peers'][value] = peer = {}
+            continue
+
+        if peer is None:
+            interface[key] = value
+        else:
+            peer[key] = value
+
+    return interface
+
+
+def _parse_interfaces(text):
+    """parses interface information from
+    the given text for multiple interfaces.
+    """
+
+    interfaces = {}
+    interface = {}
+    peer = None
+
+    for line in text.split(linesep):
+        key, value = line.strip().split(': ')
+
+        if key == 'interface':
+            interfaces[value] = interface = {'peers': {}}
+            peer = None
+            continue
+
+        if key == 'peer':
+            interface['peers'][value] = peer = {}
+            continue
+
+        if peer is None:
+            interface[key] = value
+        else:
+            peer[key] = value
+
+    return interfaces
+
+
+def show(interface='all', *, _wg=WG):
+    """Yields status information."""
+
+    if interface == 'all':
+        text = check_output((_wg, 'show', 'all'), text=True).strip()
+        return _parse_interfaces(text)
+
+    if interface == 'interfaces':
+        text = check_output((_wg, 'show', 'interfaces'), text=True).strip()
+        return text.split()
+
+    text = check_output((_wg, 'show', interface), text=True).strip()
+    return _parse_interface(text)
